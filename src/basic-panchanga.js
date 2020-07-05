68b6558255de9cedd8eeb5179c8de87ed9ae51d6
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-07-05 13:55:16 +0530

    Update basic-panchanga.js

--- src/basic-panchanga.js
+++ src/basic-panchanga.js
@@ -1,46 +1,39 @@
-var today = new Date();
-var day = today.getDay();
-var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-console.log("Today is : " + daylist[day] + ".");
-if (daylist[day]="Monday")
-{
-
-    var gulikhal = "1:30-3:00";
-    var yamaganda ="10:30-12:00";
-}
-if (daylist[day]="Tuesday")
-{
- 
-    var gulikhal = "12:00-1:30";
+var day;
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      var gulikhal = "3:00-4:30";
+      var yamaganda ="12:00-1:30";
+      break;
+    case 1:
+      day = "Monday";
+      var gulikhal = "1:30-3:00";
+      var yamaganda ="10:30-12:00";
+      break;
+    case 2:
+      day = "Tuesday";
+      var gulikhal = "12:00-1:30";
     var yamaganda ="9:00-10:30";
-}
-if (daylist[day]="Wednesday")
-{
- 
-    var gulikhal = "10:30-12:00;";
-    var yamaganda ="7:30-9:00";
-}
-if (daylist[day]="Thursday")
-{
- 
-    var gulikhal = "9:00-10:30";
-    var yamaganda = "6:00-7:30";
-}
-if (daylist[day]="Friday")
-{
- 
+      break;
+    case 3:
+      day = "Wednesday";
+      var gulikhal = "10:30-12:00;";
+      var yamaganda ="7:30-9:00";
+      break;
+    case 4:
+      day = "Thursday";
+      var gulikhal = "9:00-10:30";
+      var yamaganda = "6:00-7:30";
+      break;
+    case 5:
+      day = "Friday";    
     var gulikhal = "7:30-9:00";
     var yamaganda = "3:00-4:30";
-}
-if (daylist[day]="Saturday")
-{
-    var gulikhal = "6:00-7:30";
+      break;
+    case  6:
+      day = "Saturday";
+      var gulikhal = "6:00-7:30";
     var yamaganda ="1:30-3:00";
-}
-if (daylist[day]="Sunday")
-{
-    var gulikhal = "3:00-4:30";
-    var yamaganda ="12:00-1:30";
-}
+  }
 document.getElementById("Gulikhala").innerHTML = gulikhal;
 document.getElementById("Yamagandakhala").innerHTML = yamaganda;
