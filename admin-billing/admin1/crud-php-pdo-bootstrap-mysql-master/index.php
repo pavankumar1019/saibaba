43d7487e884a727d6603e3b65718eeb9eb973172
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-05 15:55:38 +0530

    a

--- admin-billing/admin1/crud-php-pdo-bootstrap-mysql-master/index.php
+++ admin-billing/admin1/crud-php-pdo-bootstrap-mysql-master/index.php
@@ -13,7 +13,7 @@
     <a href="add-data.php" class="btn btn-large btn-info">
         <i class="glyphicon glyphicon-plus"></i> &nbsp; Offline Data
       </a>
-      <input id="myInput" type="text" placeholder="Search..">
+      <input id="myInput"  class="form-control" type="text" placeholder="Search..">
 </div>
 <br />
 <div class="container"> 
