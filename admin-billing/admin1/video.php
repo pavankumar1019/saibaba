9c52fb79f51be698cde787ce6b63e0ac762e203b
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-05 18:50:07 +0530

    a

--- admin-billing/admin1/video.php
+++ admin-billing/admin1/video.php
@@ -116,7 +116,7 @@
           </button>
 
           <!-- Topbar Search -->
-          <!-- <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group">
               <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
               <div class="input-group-append">
@@ -125,36 +125,10 @@
                 </button>
               </div>
             </div>
-          </form> -->
+          </form>
 
           <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            <li class="nav-item dropdown no-arrow d-sm-none">
-              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-              </a>
-              <!-- Dropdown - Messages -->
-              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                <form class="form-inline mr-auto w-100 navbar-search">
-                  <div class="input-group">
-                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-
-           
-
-          </ul>
+         
 
         </nav>
         <!-- End of Topbar -->
@@ -164,12 +138,12 @@
        
           <h1 class="h3 mb-4 text-gray-800">Add Data</h1>
               <h6 class="m-0 font-weight-bold text-primary">Online Bill</h6>
-              
+<!--               
   <iframe    frameborder="0" 
     marginheight="0" 
     marginwidth="0" 
     width="100%" 
-    height="100%"  src="insert-video/index.php" allowfullscreen></iframe>
+    height="100%"  src="insert-video/index.php" allowfullscreen></iframe> -->
 </div>
     <div class="container-fluid" id="table-scroll">
     <div class="table-responsive">
