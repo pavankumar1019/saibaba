f91542658475ca765e292233bb8a919f265413f9
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-05 19:49:30 +0530

    a

--- admin-billing/admin1/online_bill.php
+++ admin-billing/admin1/online_bill.php
@@ -4,10 +4,25 @@
   Email : skhairimedaimane@gmail.com
   Repo : https://github.com/medaimane/crud-php-pdo-bootstrap-mysql
 */
+/*
+  Author : Pavan Kumar
+  Email : pavan5639kumar@gmail.com
+*/
 
-include_once 'crud-php-pdo-bootstrap-mysql-master/dbconfig.php';
+$DB_host = "localhost";
+$DB_user = "u430139865_ydyp";
+$DB_pass = "pavan5639";
+$DB_name = "u430139865_ydyp";
 
-
+try
+{
+	$DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
+	$DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+	echo $e->getMessage();
+}
 ?>
 
 
