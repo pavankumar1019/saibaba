f5951e18376042f8452e8f693cd9ec4822d9fa4f
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-05 13:06:08 +0530

    a

--- admin-billing/admin1/crud-php-pdo-bootstrap-mysql-master/crud.php
+++ admin-billing/admin1/crud-php-pdo-bootstrap-mysql-master/crud.php
@@ -1,21 +1,6 @@
 <?php
-/*
-  Author : Mohamed Aimane Skhairi
-  Email : skhairimedaimane@gmail.com
-  Repo : https://github.com/medaimane/crud-php-pdo-bootstrap-mysql
-*/
-
-/*
-	Table tbl_articles : 
-
-	@var id  primary key
-	@var title varchar
-	@var content text
-
-*/
 
 	
-
 class ArticlesCrud{
 
 	private $connection;
