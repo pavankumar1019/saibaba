606fb5a5f73a4e5fa28735016b45394692de8bef
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-05 13:45:26 +0530

    a

--- admin-billing/admin1/crud-php-pdo-bootstrap-mysql-master/crud.php
+++ admin-billing/admin1/crud-php-pdo-bootstrap-mysql-master/crud.php
@@ -28,7 +28,7 @@ class ArticlesCrud{
 
 	public function add(){
 		$this->connection->prepare();
-		$sql = "INSERT INTO tbl_Devotees_Data (first_name,last_name,email_id,contact_no,event,event_date,amount,state,district,address)
+		$sql = "INSERT INTO tbl_Devotees_Data (first_name,last_name,email_id,contact_no,event_t,event_date,amount,state_t,district,address_t)
 		VALUES ('$fname', '$lname','$email','$contact','$event','$event_date','$amount','$state','$district','$address')";
 		$DB_con->exec($sql);  
 		
