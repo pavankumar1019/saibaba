5dc8c8b44483afc30372cd9cc71dab83a81089fd
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-17 00:15:25 +0530

    a

--- dm_register.php
+++ dm_register.php
@@ -14,7 +14,7 @@ if(isset($_POST['save']))
 	 $first_name = $_POST['name'];
 	 $last_name = $_POST['desg'];
 	 $city_name = $_POST['number'];
-	 $sql = "INSERT INTO d,_devotees (name,designation,phone)
+	 $sql = "INSERT INTO dm_devotees (name,designation,phone)
 	 VALUES ('$first_name','$last_name','$city_name')";
 	 if (mysqli_query($conn, $sql)) {
 		echo "New record created successfully !";
