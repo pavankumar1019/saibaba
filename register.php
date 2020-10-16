700ab0053dbc83a55eee94e964d7cc8e45697f3a
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-16 23:20:35 +0530

    a

--- register.php
+++ register.php
@@ -21,25 +21,17 @@
     <!-- Default form login -->
 <form class="text-center border border-light p-5" action="#!">
 
-<p class="h4 mb-4">Sign in</p>
+<p class="h4 mb-4">Register !</p>
 
 <!-- Email -->
-<input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+<input type="text" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="Name">
 
 <!-- Password -->
-<input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
+<input type="text" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Designation">
 
-<div class="d-flex justify-content-around">
-    <div>
-        <!-- Remember me -->
-        <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
-            <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
-        </div>
-    </div>
-    <div>
+<input type="number" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Phone number">
         <!-- Forgot password -->
-        <a href="">Forgot password?</a>
+       
     </div>
 </div>
 
@@ -47,17 +39,8 @@
 <button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
 
 <!-- Register -->
-<p>Not a member?
-    <a href="">Register</a>
+<p>Register here for latest update from temple
 </p>
-
-<!-- Social login -->
-<p>or sign in with:</p>
-
-<a href="#" class="mx-2" role="button"><i class="fab fa-facebook-f light-blue-text"></i></a>
-<a href="#" class="mx-2" role="button"><i class="fab fa-twitter light-blue-text"></i></a>
-<a href="#" class="mx-2" role="button"><i class="fab fa-linkedin-in light-blue-text"></i></a>
-<a href="#" class="mx-2" role="button"><i class="fab fa-github light-blue-text"></i></a>
 
 </form>
 <!-- Default form login -->
