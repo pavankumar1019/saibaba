a6eba2dc1b2c1cd8b5f55cd343ecaa5235e2d780
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-17 00:49:09 +0530

    a

--- register.php
+++ register.php
@@ -46,23 +46,3 @@
 <!-- Default form login -->
 </body>
 </html>
-<?php
-$connection = mysql_connect("localhost", "u430139865_ydyp", "pavan5639"); // Establishing Connection with Server
-$db = mysql_select_db("u430139865_ydyp", $connection); // Selecting Database from Server
-if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
-$name = $_POST['name'];
-$desg = $_POST['desg'];
-$contact = $_POST['number'];
-if($name !=''||$desg !=''){
-//Insert Query of SQL
-$query = mysql_query("insert dm_devotees(name, designation, phone) values ('$name', '$desg', '$contact')");
-?>
-<script>swal("Thank you!", "Your Registration Success!", "success");</script>
-<?php
-}
-else{
-echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
-}
-}
-mysql_close($connection); // Closing Connection with Server
-?>
