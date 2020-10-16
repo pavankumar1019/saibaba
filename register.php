e85e05de0f1b2a32c0270c5f08071b5738cec70d
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-16 23:14:16 +0530

    a

---
+++ register.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <!-- Default form login -->
+<form class="text-center border border-light p-5" action="#!">
+
+<p class="h4 mb-4">Sign in</p>
+
+<!-- Email -->
+<input type="email" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="E-mail">
+
+<!-- Password -->
+<input type="password" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Password">
+
+<div class="d-flex justify-content-around">
+    <div>
+        <!-- Remember me -->
+        <div class="custom-control custom-checkbox">
+            <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+            <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+        </div>
+    </div>
+    <div>
+        <!-- Forgot password -->
+        <a href="">Forgot password?</a>
+    </div>
+</div>
+
+<!-- Sign in button -->
+<button class="btn btn-info btn-block my-4" type="submit">Sign in</button>
+
+<!-- Register -->
+<p>Not a member?
+    <a href="">Register</a>
+</p>
+
+<!-- Social login -->
+<p>or sign in with:</p>
+
+<a href="#" class="mx-2" role="button"><i class="fab fa-facebook-f light-blue-text"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fab fa-twitter light-blue-text"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fab fa-linkedin-in light-blue-text"></i></a>
+<a href="#" class="mx-2" role="button"><i class="fab fa-github light-blue-text"></i></a>
+
+</form>
+<!-- Default form login -->
+</body>
+</html>
