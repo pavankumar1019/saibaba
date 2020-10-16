19a2f00b619a9952c4f89984e7205dbd903efd6e
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-16 23:57:50 +0530

    a

--- register.php
+++ register.php
@@ -23,19 +23,19 @@
 <p class="h4 mb-4">Register !</p>
 
 <!-- Email -->
-<input type="text" id="name" class="form-control mb-4" placeholder="Name"  required>
+<input type="text" name="name" class="form-control mb-4" placeholder="Name"  required>
 
 <!-- Password -->
-<input type="text" id="desg" class="form-control mb-4" placeholder="Designation"  required>
+<input type="text" name="desg" class="form-control mb-4" placeholder="Designation"  required>
 
-<input type="number" id="number" class="form-control mb-4" placeholder="Phone number"  required>
+<input type="number" name="number" class="form-control mb-4" placeholder="Phone number"  required>
         <!-- Forgot password -->
        
     </div>
 </div>
 
 <!-- Sign in button -->
-<button class="btn btn-info btn-block my-4" id="submit" type="submit">Register</button>
+<button class="btn btn-info btn-block my-4" name="submit" type="submit">Register</button>
 
 <!-- Register -->
 <p>Register here for latest update from temple
