facfafdecf47844a9dac04e53957cbdd404d235e
pavankumar1019 <67842709+pavankumar1019@users.noreply.github.com>
2020-10-16 23:43:16 +0530

    a

--- register.php
+++ register.php
@@ -1,9 +1,31 @@
+<?php
+$connection = mysql_connect("localhost", "u430139865_ydyp", "pavan5639"); // Establishing Connection with Server
+$db = mysql_select_db("u430139865_ydyp", $connection); // Selecting Database from Server
+if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
+$name = $_POST['name'];
+$desg = $_POST['desg'];
+$contact = $_POST['number'];
+if($name !=''||$desg !=''){
+//Insert Query of SQL
+$query = mysql_query("insert dm devotees(name, designation, phone) values ('$name', '$desg', '$contact')");
+?>
+<script>swal("Thank you!", "Your Registration Success!", "success");</script>
+<?php
+}
+else{
+echo "<p>Insertion Failed <br/> Some Fields are Blank....!!</p>";
+}
+}
+mysql_close($connection); // Closing Connection with Server
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register!</title>
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -19,31 +41,31 @@
 </head>
 <body>
     <!-- Default form login -->
-<form class="text-center border border-light p-5" action="#!">
+<form class="text-center border border-light p-5" action="#" method="post">
 
 <p class="h4 mb-4">Register !</p>
 
 <!-- Email -->
-<input type="text" id="defaultLoginFormEmail" class="form-control mb-4" placeholder="Name">
+<input type="text" id="name" class="form-control mb-4" placeholder="Name">
 
 <!-- Password -->
-<input type="text" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Designation">
+<input type="text" id="desg" class="form-control mb-4" placeholder="Designation">
 
-<input type="number" id="defaultLoginFormPassword" class="form-control mb-4" placeholder="Phone number">
+<input type="number" id="number" class="form-control mb-4" placeholder="Phone number">
         <!-- Forgot password -->
        
     </div>
 </div>
 
 <!-- Sign in button -->
-<button class="btn btn-info btn-block my-4" type="submit">Register</button>
+<button class="btn btn-info btn-block my-4" id="submit" type="submit">Register</button>
 
 <!-- Register -->
 <p>Register here for latest update from temple
 </p>
 
 </form>
-<script>swal("Good job!", "You clicked the button!", "success");</script>
+
 <!-- Default form login -->
 </body>
 </html>
