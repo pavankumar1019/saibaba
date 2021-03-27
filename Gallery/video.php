9f52cf405ec4f6f4c30e595edfa9193726e4bcc2
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-27 14:04:01 +0530

    Update video.php

--- Gallery/video.php
+++ Gallery/video.php
@@ -6,17 +6,14 @@
         <title>Shri Shirdi Sai Baba Temple</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-        <link rel="stylesheet" href="../css/bootstrap.min.css">
-        <link rel="stylesheet" href="../css/bootstrap-theme.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="../styles/style.css">
+        
         <link rel="stylesheet" href="../css/fontAwesome.css">
         <link rel="stylesheet" href="../css/light-box.css">
         <link rel="stylesheet" href="../css/templatemo-style.css">
-
-        <link href="https://fonts.googleapis.com/css?family=Kanit:100,200,300,400,500,600,700,800,900" rel="stylesheet">
-
-        <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+          <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
 
         <style>
             .fa {
