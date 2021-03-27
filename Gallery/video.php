597f9a12a63920e039152ef983083dedeee16af7
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-27 14:00:56 +0530

    a
    a

--- Gallery/video.php
+++ Gallery/video.php
@@ -152,33 +152,40 @@
 
 <body>
 
-    <nav>
-        <div class="logo">
-            <a href="index.html">Video-<em>Gallery</em></a>
-        </div>
-      <div class="menu-icon">
-        <span></span>
-      </div>
-    </nav>
-
-    <div id="video-container">
-        <div class="video-overlay"></div>
-        <div class="video-content">
-            <div class="inner">
-              <h1 style="font-size: 8vw;"><em>Shri Shirdi Sai Baba Temple</em></h1>
-              <!-- <p>FREE CSS TEMPLATE by templatemo</p> -->
-              <div><img class="om" src="img/om.png" width="150px"></div>
-              <p>Om Sai Ram</p>
-                <div class="scroll-icon">
-                    <a class="scrollTo" data-scrollTo="portfolio" href="#"><img src="img/scroll-icon.png" alt=""></a>
-                </div>    
-            </div>
-        </div>
-        <video autoplay="" loop="" muted>
-        	<source src="saibaba.mp4" type="video/mp4" />
-        </video>
-    </div>
-
+<nav class="navbar navbar-dark " style="background: #F95700FF;">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <img src="../assets/baba2.png" width="70" alt="" srcset="">
+  </div>
+</nav>
+<div class="collapse" id="navbarToggleExternalContent">
+  <div class=" p-4" style="background: #F2AA4CFF;">
+    <h5 class="text-white h4">DM Shri Shirdi Sai Baba Temple</h5>
+    <span class="text-muted">Opp Old Food godown,KGF main Road,Bangarpet 563114,karnataka,India</span>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+          <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Introduction</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#aboutTemple">About Temple</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"aria-disabled="true">Image Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#" aria-disabled="true">Video Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#" aria-disabled="true">Online Services</a>
+        </li>
+      </ul>
+  </div>
+</div>
 
     <div class="grid-portfolio" id="portfolio">
         <div class="container">
@@ -240,89 +247,6 @@ $conn->close();
     </footer>
 
 
-     <!-- Modal button -->
-     <div class="popup-icon">
-        <button id="modBtn" class="modal-btn"><img src="img/contact-icon.png" alt=""></button>
-      </div>  
-  
-      <!-- Modal -->
-      <div id="modal" class="modal">
-        <!-- Modal Content -->
-        <div class="modal-content">
-          <!-- Modal Header -->
-          <div class="modal-header">
-            <h3 class="header-title">Say hello to <em>To Temple Priest</em></h3>
-            <div class="close-btn"><img src="img/close_contact.png" alt=""></div>    
-          </div>
-          <!-- Modal Body -->
-          <div class="modal-body">
-            <div class="col-md-6 col-md-offset-3">
-              <form id="contact" action="mail-php/mail.php" method="post">
-                  <div class="row">
-                      <div class="col-md-12">
-                        <fieldset>
-                          <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
-                        </fieldset>
-                      </div>
-                      <div class="col-md-12">
-                        <fieldset>
-                          <input name="email" type="email" class="form-control" id="email" placeholder="Your email..." required="">
-                        </fieldset>
-                      </div>
-                      <div class="col-md-12">
-                        <fieldset>
-                          <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
-                        </fieldset>
-                      </div>
-                      <div class="col-md-12">
-                        <fieldset>
-                          <button type="submit" id="form-submit" class="btn">Send Message Now</button>
-                        </fieldset>
-                      </div>
-                  </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-  
-      
-  
-      <section class="overlay-menu">
-        <div class="container">
-          <div class="row">
-            <div class="main-menu">
-                <ul>
-                    <li>
-                        <a href="index.html">Home</a>
-                    </li>
-                    <li>
-                        <a href="single-post.html">Introduction</a>
-                    </li>
-                    <li>
-                        <a href="index.html#about" >About Us</a>
-                    </li>
-                    <li>
-                        <a href="grid.php">Photo-Gallery</a>
-                    </li>
-                    <li>
-                        <a href="video.php">Video-Gallery</a>
-                    </li>
-                    <li>
-                        <a href="donate.html">Online Services</a>
-                    </li>
-                    <li>
-                    <a href="admin-uploads-to-website/">Admin login</a>
-                    </li>
-                </ul>
-                <a href="#" class="fa fa-facebook"></a>
-  <a href="#" class="fa fa-youtube"></a>
-  <a href="#" class="fa fa-instagram"></a>
- 
-                <p>Love one another and help others to rise to the higher levels, simply by pouring out love. Love is infectious and the greatest healing energy.</p>
-            </div>
-          </div>
-        </div>
       </section>
   
 
