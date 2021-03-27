d75da8f1bfc5628f5f6797dc58389be4d43073c7
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-03-27 12:37:14 +0530

    a

--- abhishekam_and _archana/db/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","u815129216_dm","Pavan5639","u815129216_dm");
-
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-
-?>
--- temp/abhishekam_and _archana/db/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli("localhost","root","Pavan1019","sai");
-
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-
-?>
