28e9e2f8059a63eddd16191d5057a912115e1c52
Pavan Kumar <67842709+pavankumar1019@users.noreply.github.com>
2021-10-13 07:58:37 +0530

    a

--- includes/footer.php
+++ includes/footer.php
@@ -48,6 +48,10 @@ document.getElementById("Yamagandakhala").innerHTML = yamaganda;
     </script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-  
+    <script>
+  $(window).load(function() {
+    $('#loading').hide();
+  });
+</script>
 </body>
 </html>
